177579dd5575f2f673abaa1d7303255fc648be9f
NikitaGamza <gamza-ns@mail.ru>
2026-09-13 01:46:33 +0400

    feat: greeting on main page

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+from src.views import process_datetime
+
+
+if __name__ == "__main__":
+    input_date = "2026-09-13 14:30:00"
+    json_result = process_datetime(input_date)
+    print(json_result)
